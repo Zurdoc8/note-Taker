f10064bad4b29af18f6e074d350a2ca51a673a1b
Andres <garelan@gmail.com>
2022-04-17 19:45:59 -0500

    added new set of apiRoutes to handle GET and POST

---
+++ Develop/routes/apiRoutes/noteRoutes.js
@@ -0,0 +1,18 @@
+const router = require('express').Router;
+
+const { notes } = require('../../db/db.json');
+
+const { noteCreateNew } = require('');
+
+router.length('/notes', (req, res) => {
+    let saved = notes;
+    res.json(saved);
+})
+
+router.post('/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+    let note = noteCreateNew(req.body, notes);
+    res.json(note);
+})
+
+module.exports = router;
